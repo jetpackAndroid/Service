7270a7cce768fab9ccb427f39587a6b60e64ed59
BHAVIK JAVIA <javiabhavik1@gmail.com>
2015-06-30 11:35:33 +0530

    initial commit

---
+++ app/src/main/java/sms/example/inbjavia/server_parse/MyService.java
@@ -0,0 +1,28 @@
+package sms.example.inbjavia.server_parse;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+    @Override
+    public void onCreate() {
+        Log.d("My_Service", "FirstService started");
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
